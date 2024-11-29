171f339963ceb1f0a15a7a1857b790d3bc0a47a3
Doxy <doxy@doxy.fyi>
2024-11-29 10:36:03 -0800

    WGSL and GLSL Support
    Adds support for parsing and generating WGSL and GLSL. SPIR-V is used as an intermediary.
    
    Thanks to SPIRV-Cross we also support generating hlsl and msl shaders.

---
+++ common.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <stdexcept>
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+
+namespace slcross {
+	struct error : std::runtime_error {
+		using std::runtime_error::runtime_error;
+	};
+
+	namespace detail {
+		inline std::string process_pragma_once(std::string data, const std::filesystem::path& path) {
+			if(auto once = data.find("#pragma once"); once != std::string::npos) {
+				std::string guard = path.string();
+				std::transform(guard.begin(), guard.end(), guard.begin(), [](char c) -> char {
+					if(!std::isalnum(c)) return '_';
+					return std::toupper(c);
+				});
+				guard = "__" + guard + "_GUARD__";
+				data.replace(once, 12, "#ifndef " + guard + "\n#define " + guard + "\n");
+				data += "#endif //" + guard + "\n";
+			}
+			return data;
+		}
+	}
+
+	inline std::string read_entire_file(std::filesystem::path path, bool support_pragma_once) {
+		std::ifstream fin(path);
+		if(!fin) throw error("Failed to open file `" + path.string() + "`... does it exist?");
+
+		fin.seekg(0, std::ios::end);
+		size_t size = fin.tellg();
+		fin.seekg(0, std::ios::beg);
+
+		std::string data(size + 1, '\n');
+		fin.read(data.data(), size);
+		if(support_pragma_once)
+			return detail::process_pragma_once(data, path);
+		return data;
+	}
+}
